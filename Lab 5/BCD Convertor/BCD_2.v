2a35524317c1387148fde71222679a8417c0ee99
Usman Aslam <53818205+usmanaslam712@users.noreply.github.com>
2020-11-21 16:44:35 +0500

    Add files via upload

---
+++ Lab 5/BCD Convertor/BCD_2.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:27:52 11/19/2020 
+// Design Name: 
+// Module Name:    BCD_2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module BCD_2 (Thousands, Hundreds, Tens, Ones, Count);
+input [11:0] Count;
+output reg [3:0] Thousands;
+output reg [3:0] Hundreds;
+output reg [3:0] Tens;
+output reg [3:0] Ones;
+
+integer i;
+
+always @ (Count)
+begin
+//tens hundreds and units will be initially placed as zero
+Thousands = 4'd0;
+Hundreds = 4'd0;
+Tens = 4'd0;
+Ones = 4'd0;
+for (i=7; i>=0; i=i-1)
+ begin 
+//Check for all columns (H,T,U) for >=5
+   if (Thousands >= 5) 
+	    Thousands = Thousands + 3;
+   if (Hundreds >= 5) 
+	    Hundreds = Hundreds + 3;
+	  if (Tens >= 5)
+	    Tens = Tens + 3;
+	   if (Ones >= 5)
+		  Ones = Ones + 3;
+		//Shift all binary digits left 1
+	 Thousands = Thousands << 1;
+	 Thousands[0] = Hundreds[3];
+	 Hundreds = Hundreds <<1;
+	 Hundreds[0] = Tens[3];
+	 Tens = Tens << 1;
+	 Tens[0] = Ones[3];
+	 Ones = Ones << 1;
+	 Ones[0] = Count[i];
+ end
+end
+
+endmodule
---
+++ Lab 5/slow clock/up_down.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:21:54 11/14/2020 
+// Design Name: 
+// Module Name:    up_down 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module up_down #(parameter n=25, parameter N=25000000)(Count,sclk,up_down,En,rst,clk);
+output reg sclk;
+output reg [n-1:0] Count;
+input up_down,En,rst,clk; //up_down is Up Down, En is Enable, rst is reset and clk is clock
+
+always @ (posedge clk or posedge rst)
+begin //begin always
+if(rst==1'b1) //if rst is 1 then all our values will be reset //rst if 
+begin
+if (up_down==1'b0) //if up_down pin is set to 0 then the counter or Count value will be set to 0 as per ascending 
+begin
+Count=0; 
+end
+else
+begin
+Count=N; //Otherwise the counter or Count value will be set to N as per descending
+end
+sclk=0; //sclk will be normally set to 0
+end //ending rst if
+
+else // rst else
+begin // rst else begin
+	if (En==1'b1) //if the Enable pin is 1 then ascending or descending can take place //Enable if
+	begin
+	
+	if (up_down==1'b0) //If up_down bit is set to zero then begin counting in ascending order
+		begin
+		Count=Count+1; //counting in ascending order
+				if(Count==N) //Once counting reaches its limit counting will be reset
+			begin
+			Count=0; //counter will be reset
+			sclk=~sclk; //slow clock will be inverted
+			end
+			
+				else
+			begin
+			Count=Count; //otherwise counting will continue
+			sclk=sclk;  //slow clock will remain in its state
+			end
+		end
+	else
+		begin
+		Count=Count-1; //counting in descending
+			if(Count==0) //once decending is complete
+			begin
+			Count=N; //Count will be set to max value
+			sclk=~sclk; //slow clock will be inverted
+			end
+			else
+			begin
+			Count=Count; //otherwise counting will continue
+			sclk=sclk; //slow clock will remain in it's state
+			end
+				end
+		end
+		else //if Enable pin is 0 // Enable else
+	begin
+	Count=Count; // Count will remain the same
+	sclk=sclk; // slow clock will remain in its present state
+	end
+end// rst else 
+end //end always
+endmodule
